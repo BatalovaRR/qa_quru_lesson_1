363524e06850d0e13c5ca42478bd0b136fa92525
degin <124191244+BatalovaRR@users.noreply.github.com>
2026-07-13 21:37:44 +0300

    Update RegistrationTests.java

--- src/test/java/RegistrationTests.java
+++ src/test/java/RegistrationTests.java
@@ -11,7 +11,7 @@ void successfulFillFormTest() {
 
     $("[id=firstName]").setValue("firstName");
     $("[id=firstName]").setValue("DDDooooouuuublefirstNamettteee");
-    $("[id=firstName]").setValue("TTThirdfirstName");
+    $("[id=firstName]").setValue("TxxTThirdfirstName");
     $("[id=lastName]").setValue("lastName");
     $("[id=userEmail]").setValue("regress19@gmail.com");
     $("[id=userNumber]").setValue("9274347088");
